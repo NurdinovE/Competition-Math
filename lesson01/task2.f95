61ae2a5af159914bbe8ba54f86dcf13520a45c45
NurdinovE <74900612+NurdinovE@users.noreply.github.com>
2024-09-13 16:49:37 +0600

    Create task2.f95

---
+++ lesson01/task2.f95
@@ -0,0 +1,15 @@
+program task2
+  implicit none
+  integer, parameter :: float64 = selected_real_kind(15, 307)
+  real(float64) :: epsilon
+  integer :: n
+  epsilon = 1.0_float64
+  n = 0.0
+  do while (1.0_float64 + epsilon > 1.0_float64)
+      epsilon = epsilon / 10.0_float64
+      n = n + 1
+  end do
+  print *, "Machine Epsilon for double precision (float64) is:", epsilon
+  print *, "Number of iterations:", n
+
+end program task2
