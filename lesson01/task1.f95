f17f57057cae2b90c423060cf3e0fb8fd9fac25b
NurdinovE <74900612+NurdinovE@users.noreply.github.com>
2024-09-13 16:49:01 +0600

    Update task1.f95

--- lesson01/task1.f95
+++ lesson01/task1.f95
@@ -1,4 +1,21 @@
-program machine_properties
+program task1
+  implicit none
+  integer, parameter :: float32 = selected_real_kind(6, 37)
+  real(float32) :: epsilon
+  integer :: n
+  epsilon = 1.0_float32
+  n = 0
+  do while (1.0_float32 + epsilon > 1.0_float32)
+      epsilon = epsilon / 10.0_float32
+      n = n + 1
+  end do
+  print *, "Machine Epsilon for single precision (float32) is:", epsilon
+  print *, "Number of iterations:", n
+
+end program task1
+
+
+program task2
   implicit none
   integer, parameter :: float64 = selected_real_kind(15, 307)
   real(float64) :: epsilon
@@ -12,4 +29,4 @@ program machine_properties
   print *, "Machine Epsilon for double precision (float64) is:", epsilon
   print *, "Number of iterations:", n
 
-end program machine_properties
+end program task2
